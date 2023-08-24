bc27a1b2d9c0329805326ccc35424bee8b4c1b4d
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-24 16:31:25 +0000

    actualizando cambios para q no salga null

--- src/js/app.js
+++ src/js/app.js
@@ -36,7 +36,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://www.linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
@@ -79,7 +79,7 @@ window.onload = function() {
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
-          ? null
+          ? ''
           : this.value == "true"
           ? true
           : this.value == "false"
